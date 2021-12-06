6c503d55e1ea60dd413b547095998c57fd432733
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-06 13:31:08 -0500

    just checking commit

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,14 +4,54 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+const validateSignup = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage(`Please Provide a First Name`),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage(`Please Provide a Last Name`),
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage(`Please confirm Password.`)
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match.');
+      }
+      return true;
+    }),
+  handleValidationErrors
+];
+
 
 // Sign up
 router.post(
   '/',
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { firstName, lastName, email, password, username } = req.body;
-    const user = await User.signup({ firstName, lastName, email, username, password });
+    const user = await User.signup({ firstName, lastName, email, username, password});
 
     await setTokenCookie(res, user);
 
