37ce293c8df3c8fa4ec790b535f58d44d181664a
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-06 13:00:42 -0500

    get session user route is working.

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -61,4 +61,19 @@ router.delete(
 }).then(res => res.json()).then(data => console.log(data));
 */
 
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
+
 module.exports = router;
