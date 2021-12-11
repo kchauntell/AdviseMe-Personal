e86b0d178dfc4cfc7e909e1b18fa58433280d6a6
Kendra Chauntell <kendrachauntell@outlook.com>
2021-12-11 16:50:59 -0500

    back to most work form. the check box needs to be marked in order to get the genre

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import PersonalNBPage from "./components/PersonalNBPage";
 import HomePage from './components/HomePage';
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-import NotebookIDPage from "./components/NotebookIdPage";
 
 function App() {
   const dispatch = useDispatch();
@@ -26,8 +25,8 @@ function App() {
           <Route exact path="/notebooks">
             <NotebooksPage />
           </Route>
-          <Route exact path={["/notebooks/:username"]}>
-            <NotebookIDPage />
+          <Route path="/notebooks/:username">
+            <PersonalNBPage/>
           </Route>
         </Switch>
       )}
